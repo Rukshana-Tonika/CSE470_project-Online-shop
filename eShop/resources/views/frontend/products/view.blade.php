865cf9af417f9fac0e25caff67cf8aecc3583353
tonika <rukshanaamintonika@gmail.com>
2022-08-11 21:25:00 +0600

    display prod details

--- eShop/resources/views/frontend/products/view.blade.php
+++ eShop/resources/views/frontend/products/view.blade.php
@@ -22,16 +22,40 @@
                 <div class="col-md-8">
                     <h2 class="mb-0">
                         {{ $products->name }}
-                        <label style="font-size: 16px;" class="float-end badge bg-danger trending_tag">Trending</label>
+                        @if($products->trending == '1')
+                           <label style="font-size: 16px;" class="float-end badge bg-danger trending_tag">Trending</label>
+                        @endif
                     </h2>
 
                     <hr>
                     <label class="me-3">Original Price : <s>Tk {{ $products->original_price }}</s></label>
                     <label class="fw-bold">Selling Price : Tk {{ $products->selling_price }}</label>
+                    <p class="mt-3 ">
+                        {!! $products->small_description !!}
+                    </p>
+                    <hr>
 
-                </div>
+                    @if($products->qty > 0)
+                        <label class="badge bg-success">In Stock</label>
+                    @else
+                        <label class="badge bg-danger">Out of Stock</label>
+                    @endif
 
-
+                    <div class="row mt-2">
+                        <div class="col-md-2">
+                            <label for="Quantity">Quantity</label>
+                            <div class="input-group text-center mb-3">
+                                <span class="input-group-text">-</span>
+                                <input type="text" name="quantity" value="1" class="form-control" />
+                                <span class="input-group-text">+</span>
+                            </div>
+                        </div>
+                        <div class="col-md-10">
+                            <br/>
+                            <button type="button" class="btn btn-success me-3 float-start">Add to Wishlist</button>
+                            <button type="button" class="btn btn-primary me-3 float-start">Add to Cart</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
